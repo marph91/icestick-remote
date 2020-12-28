fcdbe423ab61b3898720f6dd022357061bca5a24
Martin Dörfelt <martin.d@andix.de>
2020-12-28 17:10:14 +0100

    fix both failing workflows
    - Set the VHDL version to 2008.
    - Specify a tag for the ghdl docker container.

--- syn/synth.sh
+++ syn/synth.sh
@@ -22,15 +22,14 @@ rm -rf build
 mkdir -p build
 cd build
 
-ghdl -a "$ROOT/submodules/icestick-uart/hdl/uart_rx.vhd"
-ghdl -a "$ROOT/submodules/icestick-uart/hdl/uart_tx.vhd"
-ghdl -a "$ROOT/submodules/icestick-ir-encoder/hdl/codec_pkg.vhd"
-ghdl -a "$ROOT/submodules/icestick-ir-encoder/hdl/ir_encoder.vhd"
-ghdl -a "$ROOT/submodules/icestick-ir-sampler/hdl/bram.vhd"
-ghdl -a "$ROOT/submodules/icestick-ir-sampler/hdl/ir_sampler.vhd"
-ghdl -a "$ROOT/hdl/remote.vhd"
-# ghdl --synth remote
-yosys -m ghdl -p 'ghdl remote; synth_ice40 -json remote.json'
+ghdl -a --std=08 "$ROOT/submodules/icestick-uart/hdl/uart_rx.vhd"
+ghdl -a --std=08 "$ROOT/submodules/icestick-uart/hdl/uart_tx.vhd"
+ghdl -a --std=08 "$ROOT/submodules/icestick-ir-encoder/hdl/codec_pkg.vhd"
+ghdl -a --std=08 "$ROOT/submodules/icestick-ir-encoder/hdl/ir_encoder.vhd"
+ghdl -a --std=08 "$ROOT/submodules/icestick-ir-sampler/hdl/bram.vhd"
+ghdl -a --std=08 "$ROOT/submodules/icestick-ir-sampler/hdl/ir_sampler.vhd"
+ghdl -a --std=08 "$ROOT/hdl/remote.vhd"
+yosys -m ghdl -p 'ghdl --std=08 remote; synth_ice40 -json remote.json'
 nextpnr-ice40 --hx1k --package tq144 --json remote.json --pcf ../constraints/remote.pcf --asc remote.asc
 icepack remote.asc remote.bin
 
